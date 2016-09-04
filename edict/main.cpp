1d3c996c0401c3f60dc18b374eeaeaa07a1c61eb
Link <linkdoyle@163.com>
2016-09-04 14:25:42 +0800

    bugfix

--- edict/main.cpp
+++ edict/main.cpp
@@ -1,21 +1,20 @@
 ﻿#include "mainwindow.h"
+
 #include <QApplication>
 #include <QMessageBox>
-#include <QTranslator>
 
 #include "ConfigParser.h"
 int main(int argc, char* argv[]) {
+  QApplication app(argc, argv);
+
   Config& config = Config::Get();
   if (!config.load("config.json")) {
     QMessageBox::warning(nullptr, QStringLiteral("错误"),
                          QStringLiteral("读取配置文件 config.json 失败！"));
     return -1;
   }
-  QApplication app(argc, argv);
-  QTranslator qt;
-  qt.load("qt_zh_CN");
-  app.installTranslator(&qt);
-  app.setStyle("Fusion");
+
+  app.setStyle(config.basicStyle);
   MainWindow w;
   return app.exec();
 }
