6ef7fe9d6adbbde59df080537d070e3aa030fac8
Link <linkdoyle@163.com>
2016-09-04 14:19:34 +0800

    add more options to configuration

--- edict/ConfigParser.h
+++ edict/ConfigParser.h
@@ -23,11 +23,28 @@ class Config {
   bool load(const QString& filename);
   bool dump(const QString& filename) const;
   QVector<Dictionary>& getDictionaries();
+  int conciseDictIndex() const;
+  const QString& conciseDictRegex() const;
+  void setConciseDictIndex(int);
+  void setConciseDictRegex(const QString& pattern);
+
+  QString basicStyle;
+  uint32_t historyMaxCount;
+  QStringList history;
+  struct SearchOptions {
+    bool autoCorrect : 1;
+    bool caseSensitive : 1;
+    bool reverseLookup : 1;
+    bool showSimpleDefinition : 1;
+    bool wildcardEnable : 1;
+  } searchOptions;
 
  private:
   Config();
   bool parse(const char* data);
   QVector<Dictionary> dictionaries_;
+  int conciseDictIndex_;
+  QString conciseDictRegex_;
 };
 
 #endif  // CONFIG_H
